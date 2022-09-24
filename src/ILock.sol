160bbf57a057fda64807c9d969f53a64de5698b8
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2022-09-24 15:12:30 +0530

    feat: add IUniswapV3Oracle.sol
    Signed-off-by: 0xsmrpn <samarpan_d@pp.iitr.ac.in>

--- src/ILock.sol
+++ src/ILock.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.15;
 
 /// @notice A generic interface for a contract which Implements a lock mechanism for Uniswap v3 LP tokens.
 interface ILock {
-    function LockV3NFT(uint256 _tokenId, address _quoteToken) external returns(uint256);
+    function LockV3Nft(uint256 _tokenId, address _quoteToken) external returns (uint256);
+    function Unlockv3Nft(uint256 _tokenId, address _quoteToken) external returns (bool);
 }
--- src/Lock.sol
+++ src/Lock.sol
@@ -4,9 +4,19 @@ pragma solidity ^0.8.15;
 import "solmate/auth/Owned.sol";
 import "./ILock.sol";
 
-contract Lock is Owned {
+contract Lock is ILock, Owned {
+
+
     /**
      * @notice Constructor
      */
     constructor() Owned(msg.sender){}
+
+    function LockV3Nft(uint256 _tokenId, address _quoteToken) external returns (uint256){
+
+    }
+
+    function UnlockV3Nft(uint256 _tokenId, address _quoteToken) external returns (bool){
+        
+    }
 }
